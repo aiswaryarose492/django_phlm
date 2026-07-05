63b119b56f6cf28972fed064d33c59a7117b875b
aiswaryarose492 <aiswaryarose492@gmail.com>
2026-07-05 11:54:55 +0530

    Use Render PostgreSQL database

--- phlms_project/settings.py
+++ phlms_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,14 +79,11 @@ WSGI_APPLICATION = 'phlms_project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'phlms_db',
-        'USER': 'postgres',
-        'PASSWORD': 'root25',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
